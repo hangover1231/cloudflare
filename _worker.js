6d7d062fdb6d4f66af4a2bf7e1ccf72a69b3415c
hangover1231 <hangoverback@gmail.com>
2024-05-17 16:47:31 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -3,7 +3,7 @@ import { connect } from 'cloudflare:sockets';
 
 // How to generate your own UUID:
 // [Windows] Press "Win + R", input cmd and run:  Powershell -NoExit -Command "[guid]::NewGuid()"
-let userID = '5c0bfeae-c03a-1491-fefc-e7e22f79b8ec';
+let userID = '949cde46-daa7-478b-86cd-9dcc6da61ab2';
 
 const พร็อกซีไอพีs = ['cdn.xn--b6gac.eu.org', 'cdn-all.xn--b6gac.eu.org', 'workers.cloudflare.cyou'];
 
